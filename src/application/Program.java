dfb7c169a69f84d92818320eca64ec26ec684cd4
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-07 10:09:42 -0300

    Bloco try-with-resources.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,44 +1,28 @@
 package application;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		String  path =  "C:\\Users\\User\\Documents\\Marcos\\Desenvolvimento\\ws-eclipse\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 		
-		//Scanner sc = null;
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
 			
 			while (line != null) {
 				System.out.println(line);
 				line = br.readLine();
 			}
-			
 		}
 		catch(IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
-		finally {
-			try {
-				if (br!= null) br.close();
-				if (fr!= null) fr.close();
-			}
-			catch(IOException e) {
-				e.printStackTrace();
-			}
-		}
+		
 	}
 
 }
